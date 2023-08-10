0e17feee7f72f3550aec9908b2754df6fdee36a9
PardeepKumar816 <pardeepmalhi816@gmail.com>
2023-08-10 20:20:56 +0500

    hashing

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
-const encryption = require("mongoose-encryption");
+//const encryption = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -19,7 +20,7 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encryption, {secret : process.env.SECRET, encryptedFields:["password"]});
+// userSchema.plugin(encryption, {secret : process.env.SECRET, encryptedFields:["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -40,7 +41,7 @@ app.get("/register",function(req,res){
 app.post("/register", function(req,res){
     const newUser = new User({
         email : req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save().
@@ -55,7 +56,7 @@ app.post("/register", function(req,res){
 app.post("/login", function(req, res){
     User.findOne({email : req.body.username}).
     then(foundUser =>{
-        if(foundUser.password === req.body.password){
+        if(foundUser.password === md5(req.body.password)){
             res.render("secrets");
         }else{
             res.send("No user Found");
